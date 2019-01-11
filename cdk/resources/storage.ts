03c82776516b026e1ae4bb2a011db2b7022744f8
Alex Chesters <alex.chesters@bbc.co.uk>
2019-01-11 13:54:30 +0000

    add storage

---
+++ cdk/resources/storage.ts
@@ -0,0 +1,17 @@
+import * as s3 from '@aws-cdk/aws-s3'
+import * as cdk from '@aws-cdk/cdk'
+
+export default (parent: cdk.Construct) => {
+  new s3.Bucket(parent, 'BuildArtifactsBucket', {
+    lifecycleRules: [
+      {
+        abortIncompleteMultipartUploadAfterDays: 1,
+        id: 'abort-incomplete-multipart-uploads-after-1-day'
+      },
+      {
+        expirationInDays: 7,
+        id: 'expire-all-objects-after-7-days'
+      }
+    ]
+  })
+}
--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/cdk'
 
+import Storage from './resources/storage'
 import VPC from './resources/vpc'
 
 export default class Stack extends cdk.Stack {
@@ -7,5 +8,6 @@ export default class Stack extends cdk.Stack {
     super(parent, name, props)
 
     VPC(this)
+    Storage(this)
   }
 }
