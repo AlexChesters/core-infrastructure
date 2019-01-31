f863f40f0486b4be89c8492416b626c4e8f1177d
Alex Chesters <alex.chesters@bbc.co.uk>
2019-01-31 11:45:10 +0000

    remove vpc

--- cdk/resources/vpc.ts
+++
@@ -1,4 +0,0 @@
-import * as ec2 from '@aws-cdk/aws-ec2'
-import * as cdk from '@aws-cdk/cdk'
-
-export default (parent: cdk.Construct) => new ec2.VpcNetwork(parent, 'VPC')
--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,13 +1,11 @@
 import * as cdk from '@aws-cdk/cdk'
 
 import Storage from './resources/storage'
-import VPC from './resources/vpc'
 
 export default class Stack extends cdk.Stack {
   constructor (parent: cdk.App, name: string, props?: cdk.StackProps) {
     super(parent, name, props)
 
-    VPC(this)
     Storage(this)
   }
 }
