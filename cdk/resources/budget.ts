63462edf08687832098ab6a10eda7a32116294ef
Alex Chesters <alex.chesters@bbc.co.uk>
2019-02-10 13:51:32 +0000

    add budget

---
+++ cdk/resources/budget.ts
@@ -0,0 +1,31 @@
+import * as budgets from '@aws-cdk/aws-budgets'
+import * as cdk from '@aws-cdk/cdk'
+
+export default (parent: cdk.Construct) => {
+  new budgets.CfnBudget(parent, 'MonthlyBudget', {
+    budget: {
+      budgetLimit: {
+        amount: 25,
+        unit: 'USD'
+      },
+      budgetType: 'COST',
+      timeUnit: 'MONTHLY'
+    },
+    notificationsWithSubscribers: [
+      {
+        notification: {
+          comparisonOperator: 'GREATER_THAN',
+          notificationType: 'FORECASTED',
+          threshold: 125,
+          thresholdType: 'PERCENTAGE'
+        },
+        subscribers: [
+          {
+            address: 'alex@cheste.rs',
+            subscriptionType: 'EMAIL'
+          }
+        ]
+      }
+    ]
+  })
+}
--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from '@aws-cdk/cdk'
 
+import Budget from './resources/budget'
 import Storage from './resources/storage'
 
 export default class Stack extends cdk.Stack {
   constructor (parent: cdk.App, name: string, props?: cdk.StackProps) {
     super(parent, name, props)
 
+    Budget(this)
     Storage(this)
   }
 }
