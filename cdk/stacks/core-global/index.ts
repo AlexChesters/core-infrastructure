2fe4bde50cd158604fe00286662bcddce918229c
Alex Chesters <alex.chesters@bbc.co.uk>
2019-05-24 13:25:05 +0100

    add output

--- cdk/stacks/core-global/index.ts
+++ cdk/stacks/core-global/index.ts
@@ -10,6 +10,36 @@ export default class Stack extends cdk.Stack {
     super(parent, name, props)
 
     Budget(this)
-    Permissions(this, BuildArtifactsBucketEUWest1(this))
+    const permissions = Permissions(this, BuildArtifactsBucketEUWest1(this))
+
+    new cdk.Output(this, 'CodeBuildBaseRoleOutput', {
+      description: 'ARN of an IAM Role that provides basic permissions necessary for AWS CodeBuild to function',
+      export: `${this.stackName}-codebuild-base-role-arn`,
+      value: permissions.codeBuildBaseRole.roleArn
+    })
+
+    new cdk.Output(this, 'CodeBuildBasePolicyOutput', {
+      description: 'ARN of an IAM Policy that provides basic permissions necessary for AWS CodeBuild to function',
+      export: `${this.stackName}-codebuild-base-policy-arn`,
+      value: permissions.codeBuildBasePolicy.managedPolicyArn
+    })
+
+    new cdk.Output(this, 'CodePipelineBaseRoleOutput', {
+      description: 'ARN of an IAM Role that provides basic permissions necessary for AWS CodePipeline to function',
+      export: `${this.stackName}-codepipeline-base-role-arn`,
+      value: permissions.codePipelineBaseRole.roleArn
+    })
+
+    new cdk.Output(this, 'CodePipelineBasePolicyOutput', {
+      description: 'ARN of an IAM Policy that provides basic permissions necessary for AWS CodePipeline to function',
+      export: `${this.stackName}-codepipeline-base-policy-arn`,
+      value: permissions.codePipelineBasePolicy.managedPolicyArn
+    })
+
+    new cdk.Output(this, 'CodePipelineCloudFormationRoleOutput', {
+      description: 'ARN of an IAM Role that provides the permissions CloudFormation has when running as part of a CodePipeline',
+      export: `${this.stackName}-codepipeline-cloudformation-role-arn`,
+      value: permissions.codePipelineCloudFormationRole.roleArn
+    })
   }
 }
