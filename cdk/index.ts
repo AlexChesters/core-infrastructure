eef578ae32eb6b2bf6451c6c1f1906fc4cc6961b
Alex Chesters <alex.chesters@bbc.co.uk>
2019-05-24 11:21:24 +0100

    add eu-west-1 bucket

--- cdk/index.ts
+++ cdk/index.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/cdk'
 
 import Stack from './stack'
+import CoreEUWest1Stack from './stacks/core-eu-west-1'
 import CoreGlobalStack from './stacks/core-global'
 
 class Application extends cdk.App {
@@ -8,6 +9,7 @@ class Application extends cdk.App {
     super()
 
     new Stack(this, 'core-infrastructure')
+    new CoreEUWest1Stack(this, 'core-infrastructure-eu-west-1')
     new CoreGlobalStack(this, 'core-infrastructure-global')
   }
 }
