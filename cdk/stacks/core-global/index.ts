5596f1eac35a1f9dcde3bda7feebf78f1c10ebd8
Alex Chesters <alex.chesters@bbc.co.uk>
2019-05-24 11:16:04 +0100

    project structure changes

--- cdk/stacks/core-global/index.ts
+++ cdk/stacks/core-global/index.ts
@@ -1,6 +1,6 @@
 import * as cdk from '@aws-cdk/cdk'
 
-import Budget from '../resources/budget'
+import Budget from './resources/budget'
 
 export default class Stack extends cdk.Stack {
   constructor (parent: cdk.App, name: string, props?: cdk.StackProps) {
