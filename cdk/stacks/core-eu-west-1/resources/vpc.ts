23e05be11f2f28d4a032529fb8cf0f5f0da0b472
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2019-12-16 11:34:47 +0000

    add ipv6 support

--- cdk/stacks/core-eu-west-1/resources/vpc.ts
+++ cdk/stacks/core-eu-west-1/resources/vpc.ts
@@ -2,7 +2,7 @@ import * as ec2 from '@aws-cdk/aws-ec2'
 import * as cdk from '@aws-cdk/core'
 
 export default (parent: cdk.Construct): ec2.Vpc => {
-  return new ec2.Vpc(
+  const vpc = new ec2.Vpc(
     parent,
     'vpc',
     {
@@ -17,4 +17,32 @@ export default (parent: cdk.Construct): ec2.Vpc => {
       ]
     }
   )
+
+  new ec2.CfnVPCCidrBlock(
+    parent,
+    'vpccidrblock',
+    {
+      amazonProvidedIpv6CidrBlock: true,
+      vpcId: vpc.vpcId
+    }
+  )
+
+  new ec2.CfnSubnetCidrBlock(
+    parent,
+    'ipv6cidr1',
+    {
+      subnetId: vpc.publicSubnets[0].subnetId,
+      ipv6CidrBlock: '2a05:d018:979:3c00::/64'
+    }
+  )
+  new ec2.CfnSubnetCidrBlock(
+    parent,
+    'ipv6cidr2',
+    {
+      subnetId: vpc.publicSubnets[1].subnetId,
+      ipv6CidrBlock: '2a05:d018:979:3c01::/64'
+    }
+  )
+
+  return vpc
 }
