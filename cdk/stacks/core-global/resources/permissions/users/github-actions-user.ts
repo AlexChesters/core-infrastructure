13699bccb2d03e9ea831fc591630c0c3d177262c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2021-04-26 09:02:08 +0100

    add github actions user

---
+++ cdk/stacks/core-global/resources/permissions/users/github-actions-user.ts
@@ -0,0 +1,20 @@
+import * as iam from '@aws-cdk/aws-iam'
+import * as cdk from '@aws-cdk/core'
+
+export default (parent: cdk.Construct): iam.User => {
+  const statement = new iam.PolicyStatement({ effect: iam.Effect.ALLOW })
+
+  statement.addActions('sts:AssumeRole', 'sts:TagSession')
+  statement.addResources('arn:aws:iam::008356366354:role/projects-live-bucket-BucketUploadRole-1USRJUF87S00F')
+
+  return new iam.User(parent, 'GitHubActionsUser', {
+    managedPolicies: [
+      new iam.ManagedPolicy(parent, 'GitHubActionsManagedPolicy', {
+        managedPolicyName: 'github-actions-managed-policy',
+        statements: [
+          statement
+        ]
+      })
+    ]
+  })
+}
